6e7cb29b926b5dd21f24e995a9bb4b3c27760965
Jonathon Harrell <jonathonharrell@yahoo.com>
2015-07-24 10:59:55 -0500

    Forgot to run go fmt

--- dice.go
+++ dice.go
@@ -1,44 +1,44 @@
 package dice
 
 import (
-  "strings"
-  "math/rand"
-  "strconv"
-  "time"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type (
-  Dice string
-  ParsedDice struct {
-    NumDice  int
-    DiceSize int
-  }
+	Dice       string
+	ParsedDice struct {
+		NumDice  int
+		DiceSize int
+	}
 )
 
 func (dice *Dice) Parse() ParsedDice {
-  arr         := strings.Split(string(*dice), "d")
-  numDice, _  := strconv.Atoi(arr[0])
-  diceSize, _ := strconv.Atoi(arr[1])
+	arr := strings.Split(string(*dice), "d")
+	numDice, _ := strconv.Atoi(arr[0])
+	diceSize, _ := strconv.Atoi(arr[1])
 
-  return ParsedDice{numDice, diceSize}
+	return ParsedDice{numDice, diceSize}
 }
 
 func (dice *Dice) Roll() int {
-  parsed := dice.Parse()
-  min := parsed.NumDice
-  max := (parsed.NumDice * parsed.DiceSize) + 1
-  return dice.Random(min, max)
+	parsed := dice.Parse()
+	min := parsed.NumDice
+	max := (parsed.NumDice * parsed.DiceSize) + 1
+	return dice.Random(min, max)
 }
 
 func (dice *Dice) RollWithModifier(mod string) int {
-  modifier, _ := strconv.Atoi(mod)
-  return dice.Roll() + modifier
+	modifier, _ := strconv.Atoi(mod)
+	return dice.Roll() + modifier
 }
 
 func (dice *Dice) Random(min, max int) int {
-  return rand.Intn(max - min) + min
+	return rand.Intn(max-min) + min
 }
 
 func SeedRandom() {
-  rand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())
 }
